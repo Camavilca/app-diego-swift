ffac17b0a73a7bc13b557928539f8501142c3d60
Diego Martinez <diego.martinez.r@tecsup.edu.pe>
2019-09-23 17:40:50 -0500

    fix

---
+++ Tarea_Lab6/RegistrarCursoViewController.swift
@@ -0,0 +1,49 @@
+//
+//  RegistrarCursoViewController.swift
+//  Tarea_Lab6
+//
+//  Created by mbtec22 on 9/23/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class RegistrarCursoViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var txtNombreCurso: UITextField!
+    
+    @IBOutlet weak var txtNotaPracticas: UITextField!
+    
+    @IBOutlet weak var txtNotaLaboratorio: UITextField!
+    
+    @IBOutlet weak var txtNotaFinal: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+    
+    @IBAction func registrar(_ sender: Any) {
+        
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
